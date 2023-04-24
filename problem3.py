c100e2c02f285346c12b04ed1cc4fe76176fa79c
Nina Kroft <124377480+ninaKroft@users.noreply.github.com>
2023-04-24 11:27:53 -0400

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -2,4 +2,13 @@
 
 #What is the largest prime factor of the number 600851475143 ?
 
+biggestFactor = 0
 
+for x in range(1, 13195): #cant loop through 600851475143
+    if x / 2 or x / 3 != 0:
+        y = 13195 % x
+        if y == 0:
+            primeFactor = x
+            if primeFactor > biggestFactor:
+                biggestFactor = primeFactor
+print(biggestFactor)
